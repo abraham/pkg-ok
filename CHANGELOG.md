35c9a45eb528ef27ffc9dd8def0b4677da099199
typicode <typicode@gmail.com>
2018-02-18 01:26:40 +0100

    Check more fields by default, ensure correct line ending for bin files (#3)
    Check more fields by default, ensure correct line ending for bin files

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+## 2.0.0
+
+* Normalizes line ending for files declared in `bin` field
+* In addition to checking `main` and `bin`, checks `typings` and `module` fields if present
+* CLI signature has been changed
+
+## 1.0.0
+
+* Initial version
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const mock = require('mock-fs')
 const expect = require('expect')
@@ -8,7 +9,8 @@ expect(pkgOk(__dirname)).toEqual([])
 mock({
   '/A/package.json': JSON.stringify({
     main: 'unknown.js',
-    bin: 'unknown.js'
+    bin: 'unknown.js',
+    module: 'unknown.js'
   }),
   '/B/package.json': JSON.stringify({
     bin: {
@@ -17,19 +19,25 @@ mock({
     }
   }),
   '/C/package.json': JSON.stringify({
-    module: 'unknown.js'
+    foo: 'unknown.js'
   }),
   '/D/package.json': JSON.stringify({
     foo: {
       bar: 'bar',
       baz: 'baz'
     }
-  })
+  }),
+  '/E/package.json': JSON.stringify({
+    bin: 'script.js'
+  }),
+  '/E/script.js': 'foo\r\nbar',
+  '/E/another-script.js': 'baz\r\nqux'
 })
 
 expect(pkgOk(path.join('/A'))).toEqual([
   'main',
-  'bin'
+  'bin',
+  'module'
 ])
 
 expect(pkgOk(path.join('/B'))).toEqual([
@@ -37,11 +45,15 @@ expect(pkgOk(path.join('/B'))).toEqual([
   'bin.Y'
 ])
 
-expect(pkgOk(path.join('/C'), [ 'module' ])).toEqual([
-  'module'
+expect(pkgOk(path.join('/C'), { fields: [ 'foo' ] })).toEqual([
+  'foo'
 ])
 
-expect(pkgOk(path.join('/D'), [ 'foo' ])).toEqual([
+expect(pkgOk(path.join('/D'), { fields: [ 'foo' ] })).toEqual([
   'foo.bar',
   'foo.baz'
 ])
+
+pkgOk(path.join('/E'), { bin: ['another-script.js'] })
+expect(fs.readFileSync('/E/script.js')).toEqual('foo\nbar')
+expect(fs.readFileSync('/E/another-script.js')).toEqual('baz\nqux')
