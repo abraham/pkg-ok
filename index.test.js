3a007a222b55a82687b8ed2b0bb2864ffdc8fc5f
PoppinL <poppinlp@gmail.com>
2018-09-28 17:48:17 +0800

    feat: add browser field

--- index.test.js
+++ index.test.js
@@ -9,7 +9,11 @@ describe('pkg-ok', () => {
       '/A/package.json': JSON.stringify({
         main: 'unknown.js',
         bin: 'unknown.js',
-        module: 'unknown.js'
+        types: 'unknown.js',
+        typings: 'unknown.js',
+        module: 'unknown.js',
+        es2015: 'unknown.js',
+        browser: 'unknown.js'
       }),
       '/B/package.json': JSON.stringify({
         bin: {
@@ -37,7 +41,7 @@ describe('pkg-ok', () => {
   afterEach(() => mock.restore())
 
   it('checks /A', () => {
-    expect(() => pkgOk(path.join('/A'))).toThrowError(/main[\s\S]*bin[\s\S]*module/)
+    expect(() => pkgOk(path.join('/A'))).toThrowError(/main[\s\S]*bin[\s\S]*types[\s\S]*typings[\s\S]*module[\s\S]*es2015[\s\S]*browser/)
   })
 
   it('checks /B', () => {
